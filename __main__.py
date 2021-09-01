92c88015b23b6d6868b22e831752ea5afa75d9e1
unknown <latorre.rodrigo@gmail.com>
2021-08-31 21:54:17 -0400

    Eliminamos texto innecesario

--- __main__.py
+++ __main__.py
@@ -4,24 +4,6 @@ import os
 import fileinput
 
 
-text_index = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
-</head>
-<body>
-  {% load static %}
-  {{ saludo }}
-  <script src="{% static 'js/index.js' %}"></script>
-</body>
-</html>
-'''
-
-
 def add_after(filename, old_line, new_line):
     with fileinput.FileInput(filename, inplace=True, backup = '.bak') as f:
         for line in f:
